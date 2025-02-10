4316b4dba70941b2dfe2122f4fb92f5feee25d15
PeanutSoup55 <114015859+SSCsoup@users.noreply.github.com>
2025-02-09 19:41:38 -0500

    added regualr file code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Header } from './components/Header';
+import React from 'react';
+import { Balance } from './components/Balance.js';
+import { IncomeExpenses } from './components/IncomeExpenses.js';
+import { TransactionList } from './components/TransactionList.js';
+import { AddTransaction } from './components/AddTransaction.js';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
+    </>
+  );
+}
+
+export default App;
