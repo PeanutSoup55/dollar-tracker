702c36501952d42823ceeb6824986750d6fd5c85
PeanutSoup55 <114015859+SSCsoup@users.noreply.github.com>
2025-02-09 20:01:27 -0500

    reduced formatting redundancy

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,21 +1,6 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
-
-//Money formatter function
-function moneyFormatter(num) {
-  let p = num.toFixed(2).split('.');
-  return (
-    '$ ' +
-    p[0]
-      .split('')
-      .reverse()
-      .reduce(function (acc, num, i, orig) {
-        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
-      }, '') +
-    '.' +
-    p[1]
-  );
-}
+import moneyFormatter from './MoneyFormatter';
 
 export const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
@@ -35,11 +20,11 @@ export const IncomeExpenses = () => {
     <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-  <p className="money plus">{moneyFormatter(income)}</p>
+          <p className="money plus">{moneyFormatter(income)}</p>
         </div>
         <div>
           <h4>Expense</h4>
-  <p className="money minus">{moneyFormatter(expense)}</p>
+          <p className="money minus">{moneyFormatter(expense)}</p>
         </div>
       </div>
   )
---
+++ src/components/MoneyFormatter.js
@@ -0,0 +1,11 @@
+function moneyFormatter(num) {
+    let p = num.toFixed(2).split('.');
+    return (
+      '$ ' + p[0].split('').reverse().reduce(function (acc, num, i) {
+          return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+        }, '') +
+      '.' + p[1]
+    );
+  }
+  
+  export default moneyFormatter;
